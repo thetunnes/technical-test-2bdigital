34ba0e7c27ba77e89b0e98ef186727f431215245
Tunnes <cauepani06@gmail.com>
2023-10-22 21:01:06 -0300

    Refactoring unit tests in component Cart and full coverage achieved

--- src/components/Cart/index.test.tsx
+++ src/components/Cart/index.test.tsx
@@ -1,19 +1,36 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+} from '@testing-library/react'
 import { describe, expect, it, beforeEach } from 'vitest'
 import { Cart } from '.'
 import { OpenDrawerProvider } from '@/context/useOpenDrawer'
-import { useStoreCart } from '@/store/cart'
+import { ProductCart, useStoreCart } from '@/store/cart'
 
 const products = [
   {
     id: 'test-id',
     name: 'TEST',
+    amount: 1,
+    imageUrl: '',
+    maxParcels: 3,
+    price: 100,
+    selectedSize: { id: 'size-id', label: 'M', stock: 3 },
   },
-]
+] as ProductCart[]
 
 describe('Component Cart', () => {
   beforeEach(() => {
-    useStoreCart.setState({ amount: products.length })
+    const { result } = renderHook(() => useStoreCart())
+
+    act(() => {
+      result.current.amount = products.length
+      result.current.products = products
+    })
+
     render(
       <OpenDrawerProvider>
         <Cart />
@@ -30,12 +47,19 @@ describe('Component Cart', () => {
   })
 
   it('should be show amount products in Cart', () => {
-    // useStoreCart.setState({ amount: products.length })
-
+    const { result } = renderHook(() => useStoreCart())
     const btnToggleCart = screen.getByTestId('btnOpenCart')
 
-    const { amount } = useStoreCart.getState()
-
+    const { amount } = result.current
     expect(Number(btnToggleCart.textContent)).toEqual(amount)
+  })
+
+  it('should pass a function to the Drawer to close it', () => {
+    const btnCloseCart = screen.getByLabelText('Close Drawer')
+
+    fireEvent.click(btnCloseCart)
+
+    const drawer = screen.getByRole('tab')
+    expect(drawer.getAttribute('data-open')).toBe('false')
   })
 })
